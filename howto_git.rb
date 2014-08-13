8fe4aa2cf68ca5d60bdfaf5fd7cf9bc02c501388
Gibil <jrevilla55@gmail.com>
2014-08-13 17:54:44 -0500

    last

--- howto_git.rb
+++ howto_git.rb
@@ -5,24 +5,50 @@
 # Last modified:	id. 
 
 
+
 # Adding
+cd into the dir
+
 git init 
 git add .
 git status
 git commit -m "Initial commit"
 git log
+
+
 # Work
 git add . 
+git commit -m "..."
+
+
+# Remote add 
+Login and add manually. 
+git remote add origin https://github.com/gibil5/first_app.git
+
 # Push 
 git push -u origin master
+
+
+#jx: 13 aug 2014 
+# Modify in machine A, get new changes in machine B
+In machine A
+git add . 
+git commit -m ""
+git push -u origin master 
+
+Check on git site 
+
+In machine B
+git pull 
+
+
+
+
 
 
 
 # Checkout
 git checkout -f
-
-# Remote add 
-git remote add origin https://github.com/gibil5/first_app.git
 
 
 
